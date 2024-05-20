524ff0f6a31b67c0f422cfed10e3175324db5803
NIRAJ <nirarajput6983@gmail.com>
2024-05-20 13:22:40 +0530

    Initial commit

---
+++ lib/views/screen/firstscreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class firstscreen extends StatelessWidget {
+  const firstscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(child: Column());
+  }
+}
